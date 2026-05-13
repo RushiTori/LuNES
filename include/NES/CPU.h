d1920a2fea6f06414e3feb218854a83abed231c0
RushiTori <luciefontaine0507@gmail.com>
2026-05-13 17:48:14 +0200

    Added Stack manipulation and interrupt functions

--- include/NES/CPU.h
+++ include/NES/CPU.h
@@ -3,6 +3,24 @@
 
 #include "CPUMemory.h"
 
+#define CPU_FLAG_C 0
+#define CPU_FLAG_Z 1
+#define CPU_FLAG_I 2
+#define CPU_FLAG_D 3
+#define CPU_FLAG_B 4
+#define CPU_FLAG_5 5  // bit 5 is supposed to always be 1
+#define CPU_FLAG_V 6
+#define CPU_FLAG_N 7
+
+typedef enum InterruptID {
+	CPU_INT_RESET,
+	CPU_INT_NMI,
+	CPU_INT_IRQ,
+	CPU_INT_BRK,
+} InterruptID;
+
+u16 GetInterruptVector(InterruptID interrupt);
+
 typedef struct CPU {
 	CPUram memory;
 
@@ -18,5 +36,16 @@ typedef struct CPU {
 	u8 extraCycles;
 	uint64_t cycles;
 } CPU;
+
+void CPUInit(CPU* cpu);
+
+uint64_t CPUStep(CPU* cpu);
+
+void CPUStackPush(CPU* cpu, u8 value);
+void CPUStackPush16(CPU* cpu, u16 value);
+u8 CPUStackPop(CPU* cpu);
+u16 CPUStackPop16(CPU* cpu);
+
+uint64_t CPUInterrupt(CPU* cpu, InterruptID interrupt, bool pushB);
 
 #endif	// LU_NES_CPU_H
